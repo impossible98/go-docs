bbfe95188a553f750b03260852dea20eefaba19e
impossible98 <christoleavenow@outlook.com>
2024-08-28 05:31:31 +0000

    feat: add bufio

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,18 +1,43 @@
+// import built-in modules
 import * as path from 'path';
+// import third-party modules
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'Rspress',
-  description: 'Rspack-based Static Site Generator',
+  title: 'Go',
+  description: 'About Go',
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
   },
+  logoText: 'Go',
+  markdown: {
+    checkDeadLinks: true,
+  },
   themeConfig: {
+    lastUpdated: true,
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      { icon: 'github', mode: 'link', content: 'https://github.com/golang/go' },
     ],
+    footer: {
+      message:
+        '<p>Copyright © 2020-${new Date().getFullYear()} My Project, Inc. Built with Rspress</p>',
+    },
+    hideNavbar: 'auto',
+    enableScrollToTop: true,
+    sidebar: {
+      '/': [
+        {
+          text: 'INTRODUCTION',
+          link: '/index',
+        },
+        {
+          text: 'bufio',
+          link: '/bufio',
+        },
+      ],
+    },
   },
 });
